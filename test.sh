42ec578d72a28ec6646b9fac9426489b4d1b4bda
vm <rosscreaven@gmail.com>
2021-05-09 17:01:17 +0000

    updated

--- test.sh
+++ test.sh
@@ -2,12 +2,14 @@
 
 RUNS=35
 
-mkdir csvout/q2a
-mkdir csvout/q2a/${RUNS}runs
+mkdir csvout/q2a3
+mkdir csvout/q2a3/${RUNS}runs
 
 for((i=1;i<=$RUNS;i++))
 do
 
-./timer_mem_use 1000000 10 > csvout/q2a/${RUNS}runs/run${i}.csv &
+./timer_mem_use 1000000 10 > csvout/q2a3/${RUNS}runs/run${i}.csv &
 
 done
+
+vmstat 1 200 > csvout/q2a3/${RUNS}runs.txt &
