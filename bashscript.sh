a6c4bb476fd7e81be96562220a85c6031f7643d6
rosscreav <rosscreaven@gmail.com>
2021-05-09 13:07:55 +0100

    e

--- bashscript.sh
+++ bashscript.sh
@@ -4,5 +4,5 @@ mkdir csvout/q2a
 mkdir csvout/q2a/${RUNS}runs
 for (( i = 1; i <= $RUNS; i++ ))
 do
-./q2a 5 1000 > csvout/q2a/${RUNS}runs/run${i}.csv &
+./a.out 2000000 50 > csvout/q2a/${RUNS}runs/run${i}.csv &
 done
